407a9b70c96de88b7d1fac793f17c2130a88cef0
qiuxiaokun <450662262@qq.com>
2017-06-23 17:45:22 +0800

    【修复】修改无法创建token导致的错误

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,16 +1,18 @@
 # encoding: utf8
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
+# from django.conf import settings
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from rest_framework.authtoken.models import Token
 
 
+'''
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, isinstance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=isinstance)
+'''
 
 
 class Jh_User(models.Model):
