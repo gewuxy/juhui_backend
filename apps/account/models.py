485f32fe163c6b660d1f97da902430dd970cb406
qiuxiaokun <450662262@qq.com>
2017-06-23 09:34:31 +0800

    给用户添加交易密码

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,6 +1,16 @@
 # encoding: utf8
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, isinstance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=isinstance)
 
 
 class Jh_User(models.Model):
@@ -19,6 +29,7 @@ class Jh_User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_delete = models.BooleanField(default=False, verbose_name='是否无效用户')
+    trade_passwd = models.CharField(max_length=16, verbose_name='交易密码', default='')
 
     class Meta:
         db_table = 'account_jh_user'
