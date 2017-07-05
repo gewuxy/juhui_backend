99b32b2b9b100c94edd171b6ec3449af0ea4b5e0
qiuxiaokun <450662262@qq.com>
2017-07-05 10:09:07 +0800

    完善分时数据接口

--- apps/wine/wine_view_lib.py
+++ apps/wine/wine_view_lib.py
@@ -32,36 +32,44 @@ def forchart(request):
     }
     data = []
     if period == '1m':
-        try:
-            now_date = datetime.datetime.fromtimestamp(now).date()
-            wine = WineInfo.objects.get(code=wine_code)
-        except Exception:
-            return JsonResponse(get_response_data('000002'))
-        start_time = int(time.mktime(now_date.timetuple()))
-        for deal in Deal.objects.filter(
-                wine=wine, create_at__date=now_date).order_by('create_at'):
-            print('======price=====')
-            print('{0}'.format(deal.price))
-            hour = deal.create_at.hour
-            minute = deal.create_at.minute
-            timestamp = start_time + 60 * hour + minute
-            timestamp = str(timestamp)
-            if not tmp_data.get(timestamp):
-                tmp_data[timestamp] = {}
-                tmp_data[timestamp]['last_price'] = deal.price
+        delta = 1
+    elif period == '5m':
+        delta = 5
+    elif period == '15m':
+        delta = 15
+    elif period == '30m':
+        delta = 30
+    elif period == '60m':
+        delta = 60
+
+    try:
+        now_date = datetime.datetime.fromtimestamp(now).date()
+        wine = WineInfo.objects.get(code=wine_code)
+    except Exception:
+        return JsonResponse(get_response_data('000002'))
+    start_time = int(time.mktime(now_date.timetuple()))
+    for deal in Deal.objects.filter(
+            wine=wine, create_at__date=now_date).order_by('create_at'):
+        print('======price=====')
+        print('{0}'.format(deal.price))
+        hour = deal.create_at.hour
+        minute = deal.create_at.minute
+        timestamp = start_time + 60 * hour + minute // delta * delta
+        timestamp = str(timestamp)
+        if not tmp_data.get(timestamp):
+            tmp_data[timestamp] = {}
+            tmp_data[timestamp]['last_price'] = deal.price
+            tmp_data[timestamp]['high_price'] = deal.price
+            tmp_data[timestamp]['low_price'] = deal.price
+            tmp_data[timestamp]['num'] = deal.num
+            tmp_data[timestamp]['timestamp'] = timestamp
+        else:
+            tmp_data[timestamp]['num'] += deal.num
+            tmp_data[timestamp]['last_price'] = deal.price
+            if tmp_data[timestamp]['high_price'] < deal.price:
                 tmp_data[timestamp]['high_price'] = deal.price
+            if tmp_data[timestamp]['low_price'] > deal.price:
                 tmp_data[timestamp]['low_price'] = deal.price
-                tmp_data[timestamp]['num'] = deal.num
-                tmp_data[timestamp]['timestamp'] = timestamp
-            else:
-                tmp_data[timestamp]['num'] += deal.num
-                tmp_data[timestamp]['last_price'] = deal.price
-                if tmp_data[timestamp]['high_price'] < deal.price:
-                    tmp_data[timestamp]['high_price'] = deal.price
-                if tmp_data[timestamp]['low_price'] > deal.price:
-                    tmp_data[timestamp]['low_price'] = deal.price
-                tmp_data[timestamp]['timestamp'] = timestamp
-            data.append(tmp_data[timestamp])
-        return JsonResponse(get_response_data('000000', data))
-    else:
-        return JsonResponse(get_response_data('000000'))
+            tmp_data[timestamp]['timestamp'] = timestamp
+        data.append(tmp_data[timestamp])
+    return JsonResponse(get_response_data('000000', data))
