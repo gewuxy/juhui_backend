db9b5bd107f5d9ec3483a2ba5a41eedccf0f5bfd
qiuxiaokun <450662262@qq.com>
2017-07-12 16:08:21 +0800

    【功能】添加文件上传接口；给获取聊天记录接口添加分页功能

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from apps.chat.views import (
     get_comment,
+    upload,
 )
 from apps.chat.tests import (
     create_comment,
@@ -11,4 +12,5 @@ from apps.chat.tests import (
 urlpatterns = [
     url(r'^comment/$', get_comment, name='get_comment'),
     url(r'^createcomment/$', create_comment, name='create_comment'),
+    url(r'upload/$', upload, name='upload')
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -4,6 +4,7 @@ from apps.wine.models import WineInfo
 from apps import get_response_data
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from config.settings.common import ROOT_DIR
 
 
 @api_view(['GET'])
@@ -12,13 +13,19 @@ def get_comment(request):
     wine_code = request.GET.get('code')
     if not wine_code:
         return JsonResponse(get_response_data('000002'))
-    count = request.GET.get('count', 100)
+    page = request.GET.get('page', 1)
+    page_num = request.GET.get('page_num', 50)
+    # count = request.GET.get('count', 100)
     try:
-        count = int(count)
+        # count = int(count)
+        page = int(page)
+        page_num = int(page_num)
+        start = (page - 1) * page_num
+        end = page * page_num
         wine = WineInfo.objects.get(code=wine_code)
     except Exception:
         return JsonResponse(get_response_data('000002'))
-    comments = Comment.objects.filter(wine=wine).order_by('-create_at')[:100]
+    comments = Comment.objects.filter(wine=wine).order_by('-create_at')[start: end]
     data = []
     for comment in comments:
         tmp_comment = {}
@@ -31,4 +38,36 @@ def get_comment(request):
         tmp_comment['type'] = comment.type
         tmp_comment['create_at'] = comment.create_at
         data.append(tmp_comment)
+    return JsonResponse(get_response_data('000000', data))
+
+
+@api_view(['POST'])
+@permission_classes((IsADirectoryError, ))
+def upload(request):
+    file = request.FILES.get('file')
+    if not file:
+        return JsonResponse(get_response_data('200001'))
+    file_name = file.name
+    print('file name is {0}'.format(file_name))
+    try:
+        file_type = file_name.split('.')[1]
+    except Exception:
+        return JsonResponse(get_response_data('200002'))
+    if file_type in ['jpg', 'JPG', 'JPEG', 'jpeg', 'png', 'PNG']:
+        file_path = ROOT_DIR + 'media/img/' + file_name
+        media_url = request.build_absolute_uri('media/img/' + file_name)
+    elif file_type in ['mp3', 'MP3', 'amr', 'AMR']:
+        file_path = ROOT_DIR + 'media/voice/' + file_name
+        media_url = request.build_absolute_uri('media/voice/' + file_name)
+    elif file_type in ['mp4', 'MP4']:
+        file_path = ROOT_DIR + 'media/video/' + file_name
+        media_url = request.build_absolute_uri('media/video/' + file_name)
+    else:
+        return JsonResponse(get_response_data('200002'))
+    print('file path is {0}'.format(file_path))
+    with open(str(file_path), 'wb+') as f:
+        for chunk in file.chunks():
+            f.write(chunk)
+    data = {'media_url': media_url}
+
     return JsonResponse(get_response_data('000000', data))
