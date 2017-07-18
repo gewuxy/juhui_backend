957cb3a92c27e422a672012708fe6cfed263070b
qiuxiaokun <qiuxiaokun@vin-dex.com>
2017-07-18 18:06:13 +0800

    修改公众号人气数，自选数推送接口

--- apps/nodejs/index.js
+++ apps/nodejs/index.js
@@ -14,9 +14,10 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 var code = ""
 app.get('/', function(req, res){
     //res.sendFile(__dirname + '/index.html');
+    console.log('enter app.get / ')
     code = req.query.code;
     console.log(code);
-   res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/last_price/', function(req, res){
@@ -39,17 +40,37 @@ app.post('/send_msg/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
-    /***
+    code = socket.handshake.query.code;
+    console.log('code is ' + code);
+    // 推送人气数和自选数
+    popularity_key = 'juhui_chat_popularity_' + code
+    popularity_val = client.get(popularity_key)
+    if (popularity_val){
+        popularity_val = Number(popularity_val) + 1
+    }else{
+        popularity_val = 1
+    }
+    client.set(popularity_key, popularity_val);
+    select_key = 'juhui_chat_select_' + code;
+    select_val = client.get(select_key);
+    if (select_val){
+        select_val = Number(select_val);
+    }else{
+        select_val = 0;
+    }
+    popular_select_data = {'is_msg': '0', 'popularity': popularity_val, 'select': select_val};
+    io.emit(code, popular_select_data);
+    console.log('emit popular_select_data: ' + popular_select_data)
+    
     socket.on(code, function(msg){
+        msg['is_msg'] = '1'
         io.emit(code, msg);
-        //client.set('nodejs_test', msg, redis.print)
-        client.publish('save_msg', JSON.stringify(msg))
-        //client.publish('save_msg', msg.toString('utf8'))
-        console.log('message: ' + msg + '===' + code + '====');
+        client.publish('save_msg', JSON.stringify(msg));
+        console.log('message: ' + JSON.stringify(msg) + '===' + code + '====');
     });
-    ***/
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        io.emit(code, {'is_msg': '0', 'popularity': '-1', 'select': '0'});
     });
 });
 
